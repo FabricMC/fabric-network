4593c04acc4eb08272bf52c87240a366ebc36685
asiekierka <asiekierka@gmail.com>
2016-08-25 21:14:54 +0200

    fix license headers, update mappings

--- src/main/java/net/fabricmc/network/impl/SimpleAbstractChannel.java
+++ src/main/java/net/fabricmc/network/impl/SimpleAbstractChannel.java
@@ -1,3 +1,19 @@
+/*
+ * Copyright 2016 FabricMC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package net.fabricmc.network.impl;
 
 import net.fabricmc.api.Side;
@@ -51,7 +67,7 @@ public abstract class SimpleAbstractChannel<T extends AbstractPacket> extends Ab
 
     public void sendToAllInRadius(T packet, WorldServer world, Vec3d pos, double radius) {
         double maxDist = radius * radius + radius * radius + radius * radius;
-        sendToAll(packet, world.b(EntityPlayerServer.class,
+        sendToAll(packet, world.getPlayers(EntityPlayerServer.class,
                 player -> player.getDistanceTo(pos.x, pos.y, pos.z) <= maxDist));
     }
 
--- src/test/java/net/fabricmc/network/mixin/TestMixin.java
+++ src/test/java/net/fabricmc/network/mixin/TestMixin.java
@@ -22,17 +22,18 @@ import net.fabricmc.network.test.TestPacket;
 import net.minecraft.block.IBlockState;
 import net.minecraft.block.impl.BlockDirt;
 import net.minecraft.client.world.WorldClient;
-import net.minecraft.entity.player.EntityPlayerAbstract;
+import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.util.Hand;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.Facing;
+import net.minecraft.world.World;
 import org.spongepowered.asm.mixin.Mixin;
 
 @Mixin(value = BlockDirt.class, remap = false)
 public abstract class TestMixin {
 
 
-	public boolean activate(WorldClient world, BlockPos pos, IBlockState state, EntityPlayerAbstract player, Hand hand, Facing facing, float hitX, float hitY, float hitZ) {
+	public boolean activate(World world, BlockPos pos, IBlockState state, EntityPlayer player, Hand hand, Facing facing, float hitX, float hitY, float hitZ) {
 		if (world.isRemote) {
 			NetworkTest.channel.sendToServer(new TestPacket(42));
 			NetworkTest.channel.sendToServer(new TestAsyncPacket(69));
